0653d321fb3a96c2ce2e145959cbc3166c83296f
ShivantGupta <87853578+ShivantGupta@users.noreply.github.com>
2023-03-22 07:13:49 +0530

    My First commit

---
+++ template-contact.php
@@ -0,0 +1,182 @@
+<?php
+ // Template Name: contact us
+
+get_header();
+  ?>
+ 
+  	
+ 
+	<div class="container">
+		
+		<div class="left">
+			<img src="<?php echo get_template_directory_uri();?>/images/banner.png">
+		</div>
+		<div class="right">
+			<p><p style="color:black">I am a</p>
+		<h1> Gym Trainer</h1>
+		<p><p style="color:black">From Jhansi,doing gym is good for healthy life. it helps our body to fit. it release stress from our mind.</p>
+		<button class="btn">Join us</button>
+		</div>
+	</div>
+
+
+
+
+<section id="about">
+	<div class="about-row">
+		<div class="about-left-col">
+			<h1> About <span id="blue">me</span></h1>
+			<p id="p-title">I am a Gym Trainer</p>
+			<p>
+				From Jhansi,doing gym is good for healthy life. it helps our body to fit. it release stress from our mind.From Jhansi,doing gym is good for healthy life. it helps our body to fit. it release stress from our mind.
+
+			</p>
+			<p>
+				From Jhansi,doing gym is good for healthy life.
+			</p>
+			<button class="c-btn">Read more</button>
+		</div>
+		<div class="about-right-col">
+			<img src="<?php echo get_template_directory_uri();?>/images/img1.jpg">
+		</div>
+	</div>
+</section>
+<section id="signup">
+	<div class="signup-row">
+		<div class="signup-left-col">
+			<img src="<?php echo get_template_directory_uri();?>/images/img2.png">
+		</div>
+		<div class="signup-right-col">
+				<h1> BEING <span id="blue">BODY</span></h1>
+			<h2>BUILDER</h2>
+			<button class="c-btn">Signup</button>
+
+		</div>
+	</div>
+</section>
+<section id="services">
+	<div class="services-info">
+			<h1>Our <span id="blue">Services</span></h1>
+	<p>What we Provide</p>
+	</div>
+	<div class="services-row">
+		<div class="services-box">
+				<i class="fa fa-suitcase" aria-hidden="true"></i>
+				<h2>Cardio</h2>
+		<p>	From Jhansi,doing gym is good for healthy life. </p>
+		</div>
+
+		<div class="services-box">
+				<i class="fa fa-suitcase" aria-hidden="true"></i>
+				<h2>Cardio</h2>
+		<p>	From Jhansi,doing gym is good for healthy life. </p>
+		</div>
+
+		<div class="services-box">
+				<i class="fa fa-suitcase" aria-hidden="true"></i>
+				<h2>Cardio</h2>
+		<p>	From Jhansi,doing gym is good for healthy life. </p>
+		</div>
+
+		<div class="services-box">
+				<i class="fa fa-suitcase" aria-hidden="true"></i>
+				<h2>Cardio</h2>
+		<p>	From Jhansi,doing gym is good for healthy life. </p>
+		</div>
+
+		<div class="services-box">
+				<i class="fa fa-suitcase" aria-hidden="true"></i>
+				<h2>Cardio</h2>
+		<p>	From Jhansi,doing gym is good for healthy life. </p>
+		</div>
+
+		<div class="services-box">
+				<i class="fa fa-suitcase" aria-hidden="true"></i>
+				<h2>Cardio</h2>
+		<p>	From Jhansi,doing gym is good for healthy life. </p>
+		</div>
+
+	</div>
+</section>
+
+
+<section id="members">
+		<div class="services-info">
+			<h1>Our <span id="blue">Members</span></h1>
+	<p>What our gym member says</p>
+</div>
+
+
+<div class="member-card">
+	
+
+	<img src="<?php echo get_template_directory_uri();?>/images/member.png">
+	<p>
+ 			From Jhansi,doing gym is good for healthy life. it helps our body to fit. it release stress from our mind.From Jhansi,doing gym is good for healthy life. it helps our body to fit. it release stress from our mind.
+	</p>
+	<h2>* Shivani gupta, Gym Client</h2>
+
+	<img src="<?php echo get_template_directory_uri();?>/images/icon.png">
+</div>
+</section>
+
+<section id="work">
+		<div class="services-info">
+			<h1>Our <span id="blue">Works</span></h1>
+	<p>What we Provide</p>
+</div>
+<div class="services-row">
+	<div class="work-box">
+		<img src="<?php echo get_template_directory_uri();?>/images/ex1.jpg">
+	</div>
+	<div class="work-box">
+		<img src="<?php echo get_template_directory_uri();?>/images/ex2.jpg">
+	</div>
+	<div class="work-box">
+		<img src="<?php echo get_template_directory_uri();?>/images/ex3.jpg">
+	</div>
+	<div class="work-box">
+		<img src="<?php echo get_template_directory_uri();?>/images/ex4.jpg">
+	</div>
+	<div class="work-box">
+		<img src="<?php echo get_template_directory_uri();?>/images/ex5.jpg">
+	</div>
+	<div class="work-box">
+		<img src="<?php echo get_template_directory_uri();?>/images/ex6.jpg">
+	</div>
+</div>
+</section>
+
+<section id="contact">
+		<div class="services-info">
+			<h1>Get in<span id="blue">Touch</span></h1>
+	<p> We are Available</p>
+</div>
+
+<div class="contact-row">
+	<div class="contact-left-col">
+		<div class="form">
+			<input type="text" name="" placeholder="E-mail">
+			<input type="text" name="" placeholder="Subject"><br>
+			<textarea rows="10" cols="40" placeholder="Message"></textarea>
+			<br> <button class="c_btn">Send</button>
+		</div>
+	</div>
+		<div class="contact-right-col">
+			<h1><i class="fa fa-envelope-o" aria-hidden="true"></i> E-mail</h1> <p>shivantgupta123@gmail.com</p><br>
+ 			<h1><i class="fa fa-phone" aria-hidden="true"></i> Mobile</h1><p>8934031899, 9305449829</p>
+ 			<br>
+ 			<h1><i class="fa fa-location-arrow" aria-hidden="true"></i> Address</h1>
+ 			 <p> Near Master colony <br>
+ 			 	House no 963, baragoan gate bhara chuna bhatti sudram colony , Jhansi
+ 			 </p>
+		</div>
+</div>
+
+
+  <?php
+ 
+
+get_footer();
+
+  ?>
